5e33aabd063c5cce09eb51bf2d58fa53981f34de
Thomas <thomas.367@hotmail.com>
2022-08-28 17:33:23 +0300

    add min binary heap data structure

--- Other/javascript/Algorithms and Data Structures/Data Structures/Binary Heaps/min-binary-heap.js
+++ Other/javascript/Algorithms and Data Structures/Data Structures/Binary Heaps/min-binary-heap.js
@@ -1,23 +1,23 @@
 /**
  * Heaps are another type of tree that have some particular rules. There are two main types of heaps, 
- * MaxHeaps and MinHeaps. In MaxHeaps, parent nodes are always greater than its children.
+ * MaxHeaps and MinHeaps. In MinHeaps, parent nodes are always smaller than its children.
  * In this data structure there're no guarantees between siblings, meaning that nodes at the same 
  * "level" don't follow any rule besides being higher/lower than their parent.
  */
 
-class MaxBinaryHeap {
+class MinBinaryHeap {
     constructor() {
         this.values =[];
     }
 
-    // The insert method adds a new value to  the heap.
+    // Th einsert method adds a new value to  the heap.
     insert(value) {
         this.values.push(value);
 
         let index = this.values.length - 1;
         let parentIndex = Math.floor( (index - 1)/2 );
 
-        while (this.values[parentIndex] < this.values[index]) { 
+        while (this.values[parentIndex] > this.values[index]) { 
             [this.values[parentIndex], this.values[index]] = [this.values[index], this.values[parentIndex]];
             index = parentIndex;
             parentIndex = Math.floor( (index - 1)/2 );
@@ -26,8 +26,8 @@ class MaxBinaryHeap {
         return this.values;
     }
 
-    // The extractMax method removes the maximum value from the heap
-    extractMax() {
+    // The extractMin method removes the minimum value from the heap
+    extractMin() {
         if (this.values.length === 0) {
             return false;
         }
@@ -40,10 +40,10 @@ class MaxBinaryHeap {
             let leftChildIdx = 2 * parentIndex + 1;
             let rightChildIdx = 2 * parentIndex + 2;
 
-            if (this.values[parentIndex] < this.values[leftChildIdx] && this.values[leftChildIdx] > this.values[rightChildIdx]) {
+            if (this.values[parentIndex] > this.values[leftChildIdx] && this.values[leftChildIdx] < this.values[rightChildIdx]) {
                 [this.values[parentIndex], this.values[leftChildIdx]] = [this.values[leftChildIdx], this.values[parentIndex]];
                 parentIndex = leftChildIdx;
-            } else if (this.values[parentIndex] < this.values[rightChildIdx] && this.values[leftChildIdx] < this.values[rightChildIdx]) {
+            } else if (this.values[parentIndex] > this.values[rightChildIdx] && this.values[leftChildIdx] > this.values[rightChildIdx]) {
                 [this.values[parentIndex], this.values[rightChildIdx]] = [this.values[rightChildIdx], this.values[parentIndex]];
                 parentIndex = rightChildIdx;
             } else {
